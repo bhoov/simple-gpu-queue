f7629364ff9fde9c8d5867bda7fd53475fc827b4
Benjamin Hoover <benjamin.hoover@ibm.com>
2024-08-07 15:33:16 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Simple GPU Queue
+> Simple GPU Queue: For running lots of smaller experiments on your handful of GPUs
+> 
+
+
+## For Development
+```
+conda env create -f environment.yml
+conda activate simple-gpu-queue
+pip install -r requirements.txt
+```
---
+++ test_queue.py
@@ -0,0 +1,16 @@
+from simple_gpu_queue import GPUQueueClient
+queue = GPUQueueClient(git_commit_id="most_recent")
+
+# Send commands as a list of (command, stdout_file) tuples. Stdoutfile can be "" for no saving of stdout logging.
+commands = [
+    ("python -c 'import time; import jax.numpy as jnp; a=jnp.ones(5); time.sleep(1); print(\"I SLEPT HAPPY 1\")'", "testlogs/v1.txt"),
+    ("python -c 'import time; import jax.numpy as jnp; a=jnp.ones(5); time.sleep(2); print(\"I SLEPT HAPPY 2\")'", ""),
+    ("python -c 'import time; import jax.numpy as jnp; a=jnp.ones(5); time.sleep(3); print(\"I SLEPT HAPPY 3\")'", "testlogs/v3.txt"),
+]
+
+if queue.git_repo.is_dirty():
+    user_input = input("Repo has uncommitted changes: continue? [Y/n] ").strip().lower()
+    if user_input == 'n':
+        raise ValueError("Aborting due to uncommitted changes in the repository.") 
+
+queue.send_command_list(commands)
